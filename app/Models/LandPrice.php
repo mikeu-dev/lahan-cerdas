34fe4a511684ae0918853d005d855b496557ffb6
rikiruswandii <rikiruswandi28@gmail.com>
2025-09-14 22:37:35 +0700

    feat:schemas

---
+++ app/Models/LandPrice.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class LandPrice extends Model
+{
+    //
+}
---
+++ database/migrations/2025_09_14_151301_create_roads_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('roads', function (Blueprint $table) {
+            $table->id();
+            $table->string('nama_jalan', 150)->nullable();
+            $table->string('klasifikasi', 50)->nullable();
+            $table->geometry('geom', 'LINESTRING', 4326)->nullable();
+            $table->foreignId('created_by')->nullable()->constrained('users');
+            $table->foreignId('updated_by')->nullable()->constrained('users');
+            $table->timestamps();
+
+            $table->spatialIndex('geom');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('roads');
+    }
+};

909ccdadc4b6bb0bc5ad6c0f925f9d5123110a65
Mikeu Dev <rikiruswandi28@gmail.com>
2025-11-27 11:32:26 +0700

    feat(menus):nav group pendukung

--- app/Filament/Resources/Favorites/FavoriteResource.php
+++ app/Filament/Resources/Favorites/FavoriteResource.php
@@ -15,9 +15,11 @@ use Filament\Resources\Resource;
 use Filament\Schemas\Schema;
 use Filament\Support\Icons\Heroicon;
 use Filament\Tables\Table;
+use UnitEnum;
 
 class FavoriteResource extends Resource
 {
+    protected static string|UnitEnum|null $navigationGroup = 'Pendukung';
     protected static ?string $model = Favorite::class;
 
     protected static string|BackedEnum|null $navigationIcon = Heroicon::OutlinedRectangleStack;
--- app/Filament/Resources/Notifications/NotificationResource.php
+++ app/Filament/Resources/Notifications/NotificationResource.php
@@ -15,9 +15,11 @@ use Filament\Resources\Resource;
 use Filament\Schemas\Schema;
 use Filament\Support\Icons\Heroicon;
 use Filament\Tables\Table;
+use UnitEnum;
 
 class NotificationResource extends Resource
 {
+    protected static string|UnitEnum|null $navigationGroup = 'Pendukung';
     protected static ?string $model = Notification::class;
 
     protected static string|BackedEnum|null $navigationIcon = Heroicon::OutlinedRectangleStack;
